ca693d5aca95441cbd23c2c9a52f5c474b1677d1
Guilherme Henrique Ferreira Kasemirski <134551505+GuilhermeHFK@users.noreply.github.com>
2023-08-14 15:24:17 -0300

    Add files via upload

---
+++ pessoal/inc/cabecalho.inc.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+ <html lang="pt-br">
+<head>
+    <title>LightYear</title>
+     <meta charset="UTF-8">
+     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+  <link rel="stylesheet" type="text/css" href="css/estilo.css">
+</head>
+<body>
+    <header>
+        <a href="index.php"><img src="img/logo.png" alt="logo" width="10%"></a>
+
+        <nav class="menu_opcoes">
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Voos</a></li>
+                <li><a href="#">Sobre nós</a></li>
+                <li><a href="#">Conta</a></li>
+                <li><a href="#">Contato</a></li>
+            </ul>
+        </nav>
+    </header>
---
+++ pessoal/index.php
@@ -0,0 +1,15 @@
+<?php include 'inc/cabecalho.inc.php';?>
+
+<div class="destaque">
+    
+</div>
+
+<div class="sobre">
+    
+</div>
+
+<div class="corpo">
+    
+</div>
+
+ <?php include 'inc/rodape.inc.php';?>
